e56dfc3c9d15440b371a9b368c42e65394ca3b52
Gumenchuk <125121484+Gumenchuk@users.noreply.github.com>
2023-04-28 18:40:31 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('hello')
+const toggleBtn = document.querySelector("#toggle-theme");
+toggleBtn.addEventListener("click", function() {
+    if(document.documentElement.hasAttribute("theme")){
+        document.documentElement.removeAttribute("theme");
+    }
+    else{
+        document.documentElement.setAttribute("theme", "light");
+    }
+});
